5f9e53a47df1f257698b20fd345b4302ecaad37c
sjcl <fruit@bebehp.com>
2017-09-11 03:25:09 +0900

    traversal

--- src/main/java/net/teamfruit/factorioforge/ui/UILoginController.java
+++ src/main/java/net/teamfruit/factorioforge/ui/UILoginController.java
@@ -5,22 +5,48 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class UILoginController {
 	@FXML
 	private AnchorPane rootpane;
+	@FXML
+	private TextField username;
+	@FXML
+	private PasswordField password;
 
 	@FXML
 	private void initialize() {
-
+		this.rootpane.setOnKeyPressed(event -> {
+			if (event.getCode()==KeyCode.ENTER)
+				login(event.getSource());
+		});
+		this.username.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+			if (event.getCode()==KeyCode.TAB&&event.isShiftDown()) {
+				event.consume();
+				this.password.requestFocus();
+			}
+		});
+		this.password.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+			if (event.getCode()==KeyCode.TAB) {
+				event.consume();
+				this.username.requestFocus();
+			}
+		});
 	}
 
 	@FXML
 	private void onLogin(final ActionEvent event) throws IOException {
-		final Object o = event.getSource();
+		login(event.getSource());
+	}
+
+	private void login(final Object o) {
 		if (o instanceof Node) {
-			final UIRootController root = UI.ROOT.get(event.getSource());
+			final UIRootController root = UI.ROOT.get(o);
 			root.getChildren().remove(this.rootpane);
 		}
 	}

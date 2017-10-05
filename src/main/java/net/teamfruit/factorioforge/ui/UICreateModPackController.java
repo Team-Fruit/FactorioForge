0400c3f99dfcb4c08594e71541df029ec40e64b7
sjcl <fruit@bebehp.com>
2017-10-06 04:34:06 +0900

    #

--- src/main/java/net/teamfruit/factorioforge/ui/UICreateModPackController.java
+++ src/main/java/net/teamfruit/factorioforge/ui/UICreateModPackController.java
@@ -1,5 +1,15 @@
 package net.teamfruit.factorioforge.ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
+
 public class UICreateModPackController {
 
+	@FXML
+	private TextField name;
+	@FXML
+	private TextField author;
+	@FXML
+	private Circle circle;
 }
